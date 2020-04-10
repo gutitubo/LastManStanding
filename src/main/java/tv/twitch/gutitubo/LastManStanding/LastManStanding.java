792e4908b42f77b86289c738d1235143b044d363
ぐちつぼ <gutitubo@gmail.com>
2020-04-10 14:31:53 +0900

    Initial Commit

---
+++ src/main/java/tv/twitch/gutitubo/LastManStanding/LastManStanding.java
@@ -0,0 +1,18 @@
+package tv.twitch.gutitubo.LastManStanding;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LastManStanding extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onDisable();
+	}
+}
