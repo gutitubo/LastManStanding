7a61be6e44f3e84dccab5ccfb34010d2ba7685f2
gutitubo <>
2020-04-15 16:45:07 +0900

    継承したクラスのメソッドを定義

--- src/main/java/tv/twitch/gutitubo/LastManStanding/LastManStanding.java
+++ src/main/java/tv/twitch/gutitubo/LastManStanding/LastManStanding.java
@@ -1,18 +1,25 @@
 package tv.twitch.gutitubo.LastManStanding;
 
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LastManStanding extends JavaPlugin {
+
+	Logger logger = Bukkit.getLogger();
 
 	@Override
 	public void onEnable() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onEnable();
+		logger.info("[LMS] plugin loaded");
 	}
 
 	@Override
 	public void onDisable() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onDisable();
+		logger.info("[LMS] plugin unloaded.");
 	}
 }
