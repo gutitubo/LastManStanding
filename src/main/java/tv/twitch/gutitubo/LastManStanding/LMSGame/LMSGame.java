25b22adfaed75b1a88fd5c5124b01d23e22d5126
gutitubo <>
2020-04-18 06:18:22 +0900

    ゲームの抽象クラスを仮作成

---
+++ src/main/java/tv/twitch/gutitubo/LastManStanding/LMSGame/LMSGame.java
@@ -0,0 +1,13 @@
+package tv.twitch.gutitubo.LastManStanding.LMSGame;
+
+public abstract class LMSGame {
+
+	private int limitTime;
+	private int readyTime;
+
+	public LMSGame() {
+		limitTime = 600;
+		readyTime = 30;
+	}
+
+}
