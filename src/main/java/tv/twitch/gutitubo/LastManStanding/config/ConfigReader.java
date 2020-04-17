9fc8856fc904185c3a3665abb475f6d58da445bb
gutitubo <>
2020-04-18 06:26:11 +0900

    Config読み取り関連のクラスを作成

---
+++ src/main/java/tv/twitch/gutitubo/LastManStanding/config/ConfigReader.java
@@ -0,0 +1,20 @@
+package tv.twitch.gutitubo.LastManStanding.config;
+
+/**
+ * configから値を読み込むためのクラス
+ * 読み込んだ値を保存するには
+ * ConfigValueクラスを使用する
+ *
+ * @author gutitubo
+ *
+ */
+public class ConfigReader {
+
+	/**
+	 * configから値を読み込んでConfigValueに格納する
+	 */
+	public void reload() {
+
+	}
+
+}
---
+++ src/main/java/tv/twitch/gutitubo/LastManStanding/config/ConfigValue.java
@@ -0,0 +1,22 @@
+package tv.twitch.gutitubo.LastManStanding.config;
+
+/**
+ * Configからの値はこのクラスに一時格納する
+ * 利用する場合はここから取得する
+ * @author gutitubo
+ *
+ */
+
+public class ConfigValue {
+
+	/**
+	 * ゲーム開始までの準備時間
+	 */
+	public static int readyTime;
+
+	/**
+	 * ゲーム開始直前の隠れる時間
+	 */
+	public static int hidingTime;
+
+}
