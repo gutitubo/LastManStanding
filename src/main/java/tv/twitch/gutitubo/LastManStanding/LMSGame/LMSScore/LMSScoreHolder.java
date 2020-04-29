fe23790ac7b11bfa03993efd100a0ea87a824583
gutitubo <>
2020-04-29 23:55:51 +0900

    ScoreHolderに各種スコアを登録できるように

--- src/main/java/tv/twitch/gutitubo/LastManStanding/LMSGame/LMSScore/LMSScoreHolder.java
+++ src/main/java/tv/twitch/gutitubo/LastManStanding/LMSGame/LMSScore/LMSScoreHolder.java
@@ -1,10 +1,61 @@
 package tv.twitch.gutitubo.LastManStanding.LMSGame.LMSScore;
 
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
+
 /**
  * Scoreを保持しておくHashMapをどうにかするクラス
  * @author gutitubo
  *
  */
 public class LMSScoreHolder {
+	/*
+	 * 各スコアを保持するためのHashMap
+	 */
+	public static HashMap<String, Integer> killHolder = new HashMap<>();
+	public static HashMap<String, Integer> scoreHolder = new HashMap<>();
+	public static HashMap<String, Integer> rankHolder = new HashMap<>();
+	public static HashMap<String, Integer> allPointHolder = new HashMap<>();
 
+	/**
+	 * Holderにスコアを登録する
+	 * 死亡時と優勝時に呼び出す。
+	 * @param score
+	 */
+	public static void registScore(LMSScore score, int rank) {
+		// Pre. Playerを取得
+		Player p = score.getPlayer();
+		String name = p.getName();
+
+		// 1. 各種ホルダーに値をput
+		killHolder.put(name, score.getKill());
+		scoreHolder.put(name, score.getScore());
+		rankHolder.put(name, rank);
+
+		// 2 allPointHolderにname登録されているか確認 されていれば加算
+		if (allPointHolder.containsKey(name)) {
+			int point = allPointHolder.get(name);
+			allPointHolder.put(name, score.getScore() + point);
+		} else {
+			allPointHolder.put(name, score.getScore());
+		}
+	}
+
+	/**
+	 * スコアホルダーのリセット
+	 */
+	public static void clear() {
+		killHolder.clear();
+		scoreHolder.clear();
+		rankHolder.clear();
+	}
+
+	/**
+	 * AllPointHolderを含む全てのスコアホルダーをりせっと
+	 */
+	public static void allClear() {
+		clear();
+		allPointHolder.clear();
+	}
 }
